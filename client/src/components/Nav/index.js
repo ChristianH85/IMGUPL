d31d543a65e89142ee27307f74750c8b95822349
Christian Henry <chenry5724@gmail.com>
2020-11-29 13:34:05 -0500

    nav changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,7 +6,7 @@ function Nav() {
     <nav id='logo' className="navbar navbar-fixed navbar-expand-lg d-flex ">
     <a className="navbar-brand"  href="/">Image Uploader</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
     </button>
     <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
       <ul className="navbar-nav ">
